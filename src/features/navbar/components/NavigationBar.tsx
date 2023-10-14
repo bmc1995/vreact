4826a02ccc1fa8be37507c24602e4b1a9757a379
Billy McCrackin <44419353+bmc1995@users.noreply.github.com>
2023-10-13 21:58:53 -0400

    feat(nav): add navbar

---
+++ src/features/navbar/components/NavigationBar.tsx
@@ -0,0 +1,53 @@
+import Box from "@mui/joy/Box";
+import List from "@mui/joy/List";
+import ListDivider from "@mui/joy/ListDivider";
+import ListItem from "@mui/joy/ListItem";
+import ListItemButton from "@mui/joy/ListItemButton";
+import Person from "@mui/icons-material/Person";
+import viteLogo from "/vite.svg";
+
+export default function NavigationBar() {
+  return (
+    <Box component="nav" aria-label="Vreact Template" sx={{ flexGrow: 1 }}>
+      <List role="menubar" variant="outlined" orientation="horizontal">
+        <ListItem role="none">
+          <ListItemButton
+            role="menuitem"
+            component="a"
+            href="#NavHomeBtn"
+            aria-label="Home"
+          >
+            <img src={viteLogo} className="logo" alt="Vite logo" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem role="none" sx={{ marginInlineStart: "auto" }}>
+          <ListItemButton role="menuitem" component="a" href="#NavBtn">
+            Products
+          </ListItemButton>
+        </ListItem>
+        <ListDivider sx={{ margin: 0 }} />
+        <ListItem role="none" sx={{ minWidth: "100px" }}>
+          <ListItemButton
+            role="menuitem"
+            component="a"
+            sx={{ justifyContent: "center" }}
+            href="#NavBtn"
+          >
+            Blog
+          </ListItemButton>
+        </ListItem>
+        <ListDivider sx={{ margin: 0 }} />
+        <ListItem role="none">
+          <ListItemButton
+            role="menuitem"
+            component="a"
+            href="#NavBtn"
+            aria-label="Profile"
+          >
+            <Person />
+          </ListItemButton>
+        </ListItem>
+      </List>
+    </Box>
+  );
+}
