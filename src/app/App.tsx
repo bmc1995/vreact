d9d34fd3839f610871022c317c8311116c785d67
Billy McCrackin <44419353+bmc1995@users.noreply.github.com>
2023-10-19 19:53:09 -0400

    chore(maint): rm unused imports

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,3 @@
-import { useState } from "react";
-import reactLogo from "../assets/react.svg";
-import viteLogo from "/vite.svg";
 import "../styles/App.css";
 import ErrorBoundary from "../features/error-boundary/components/ErrorBoundary";
 import { Layout } from "../common/Layout";
