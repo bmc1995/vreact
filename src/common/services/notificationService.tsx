8d510e628d0291967f9f0658c0e7327f99322257
Billy McCrackin <44419353+bmc1995@users.noreply.github.com>
2023-10-25 18:25:31 -0400

    refactor(notifcation): rm service

--- src/common/services/notificationService.tsx
+++
@@ -1,7 +0,0 @@
-export const showConnLostMessage = (msg: string) => {
-  console.info(`%c${msg}`, "color:red");
-};
-
-export const showConnRestoMessage = (msg: string) => {
-  console.info(`%c${msg}`, "color:lightblue");
-};
