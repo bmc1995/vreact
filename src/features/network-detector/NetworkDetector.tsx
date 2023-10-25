942f9d3dab3b79ed5de1f5a79efdc0ab006bfbca
Billy McCrackin <44419353+bmc1995@users.noreply.github.com>
2023-10-25 18:36:04 -0400

    feat(network): NetworkDetector displays toasts

--- src/features/network-detector/NetworkDetector.tsx
+++ src/features/network-detector/NetworkDetector.tsx
@@ -1,11 +1,14 @@
 import { FC, PropsWithChildren, useEffect, useRef, useState } from "react";
-import * as notificationService from "../../common/services/notificationService";
+import { useDispatch } from "react-redux";
+import { addToast } from "../../app/redux/slices/toastSlice";
 
 export const NetworkDetector: FC<PropsWithChildren<unknown>> = ({
   children,
 }) => {
   const [isDisconnected, setDisconnectedStatus] = useState(false);
   const prevDisconnectionStatus = useRef(false);
+
+  const dispatch = useDispatch();
 
   const handleConnectionChange = () => {
     setDisconnectedStatus(!navigator.onLine);
@@ -16,9 +19,25 @@ export const NetworkDetector: FC<PropsWithChildren<unknown>> = ({
     window.addEventListener("offline", handleConnectionChange);
 
     if (isDisconnected) {
-      notificationService.showConnLostMessage("Internet Connection Lost");
+      dispatch(
+        addToast({
+          color: "danger",
+          msg: "Internet Connection Lost",
+          variant: "outlined",
+          id: Math.floor(Math.random() * 10000).toString(),
+          duration: 8000,
+        })
+      );
     } else if (prevDisconnectionStatus.current) {
-      notificationService.showConnRestoMessage("Internet Connection Restored");
+      dispatch(
+        addToast({
+          color: "success",
+          msg: "Internet Connection Restored",
+          variant: "outlined",
+          id: Math.floor(Math.random() * 10000).toString(),
+          duration: 8000,
+        })
+      );
     }
 
     prevDisconnectionStatus.current = isDisconnected;
