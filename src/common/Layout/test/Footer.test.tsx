f7522da8a4852744d1bf2187eb3fe54ee59c1a0e
Billy McCrackin <44419353+bmc1995@users.noreply.github.com>
2023-11-03 23:18:49 -0400

    feat(test): add footer test

---
+++ src/common/Layout/test/Footer.test.tsx
@@ -0,0 +1,37 @@
+import { describe, it, expect, vi, beforeEach } from "vitest";
+import { render, screen } from "@testing-library/react";
+import { Footer } from "../Footer";
+
+describe("<Footer />", () => {
+  const h1Text = "The Footer";
+  beforeEach(async () => {
+    vi.clearAllMocks();
+    vi.importActual("@mui/joy");
+
+    const component = render(
+      <Footer>
+        <h1>{h1Text}</h1>
+      </Footer>
+    );
+    component.container.firstElementChild!.setAttribute(
+      "data-testid",
+      "footerTest"
+    );
+    component.debug();
+  });
+
+  it("renders the footer component", async () => {
+    const footer = await screen.findByTestId("footerTest");
+    expect(footer).toBeDefined();
+  });
+
+  it("is a `<footer />` HTML Element (Semantic HTML)", async () => {
+    const footer = await screen.findByTestId("footerTest");
+    expect(footer.tagName).toEqual("footer".toUpperCase());
+  });
+
+  it("renders contained/children components", async () => {
+    const footer = await screen.findByText(h1Text);
+    expect(footer).toBeDefined();
+  });
+});
