b9edbc7bd9939040ac31a817b7ba1946e141d911
Billy McCrackin <44419353+bmc1995@users.noreply.github.com>
2023-11-07 11:42:18 -0500

    chore(test): rm unused imports

--- src/common/Layout/test/Header.test.tsx
+++ src/common/Layout/test/Header.test.tsx
@@ -1,4 +1,4 @@
-import { describe, expect, it, vi } from "vitest";
+import { describe, expect, it } from "vitest";
 import { Header } from "../Header";
 import { render, screen } from "@testing-library/react";
 
