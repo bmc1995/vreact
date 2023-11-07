57db722fd9ad2c7f27c7cf2a44830f8586849248
Billy McCrackin <44419353+bmc1995@users.noreply.github.com>
2023-11-07 11:40:37 -0500

    refactor(netdetect): simplify component

--- src/features/network-detector/NetworkDetector.tsx
+++ src/features/network-detector/NetworkDetector.tsx
@@ -1,33 +1,30 @@
-import { FC, PropsWithChildren, useEffect, useRef, useState } from "react";
+import { FC, PropsWithChildren, useEffect, useRef } from "react";
 import { dispatchToast } from "../../common/notifications/utils/dispatchToast";
 
-export const NetworkDetector: FC<PropsWithChildren<unknown>> = ({
-  children,
-}) => {
-  const [isDisconnected, setDisconnectedStatus] = useState(false);
-  const prevDisconnectionStatus = useRef(false);
-
-  const handleConnectionChange = () => {
-    setDisconnectedStatus(!navigator.onLine);
-  };
+export const NetworkDetector: FC<PropsWithChildren> = ({ children }) => {
+  const prevConnectionStatus = useRef(navigator.onLine);
 
   useEffect(() => {
+    const handleConnectionChange = () => {
+      const connected = navigator.onLine;
+      if (connected) {
+        if (!prevConnectionStatus.current) {
+          dispatchToast("Internet Connection Restored.", "success");
+        }
+      } else {
+        dispatchToast("Internet Connection Lost.", "danger");
+      }
+      prevConnectionStatus.current = connected;
+    };
+
     window.addEventListener("online", handleConnectionChange);
     window.addEventListener("offline", handleConnectionChange);
-
-    if (isDisconnected) {
-      dispatchToast("Internet Connection Lost.", "danger");
-    } else if (prevDisconnectionStatus.current) {
-      dispatchToast("Internet Connection Restored.", "success");
-    }
-
-    prevDisconnectionStatus.current = isDisconnected;
 
     return () => {
       window.removeEventListener("online", handleConnectionChange);
       window.removeEventListener("offline", handleConnectionChange);
     };
-  }, [isDisconnected]);
+  }, []);
 
   return <>{children}</>;
 };
