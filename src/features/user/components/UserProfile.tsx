c733f78c5c15ef8984033d8859916703b676230c
Billy McCrackin <44419353+bmc1995@users.noreply.github.com>
2023-11-07 18:53:31 -0500

    feat(user-profile): add profile info

--- src/features/user/components/UserProfile.tsx
+++ src/features/user/components/UserProfile.tsx
@@ -1,13 +1,28 @@
-import { Card, CardContent, CardCover, Typography } from "@mui/joy";
+import { Email, MyLocation, Phone } from "@mui/icons-material";
+import { Avatar, Box, Stack, Typography } from "@mui/joy";
 
 export default function UserProfile() {
   return (
-    <Card title="User Profile Card">
-      <CardCover>
-        <CardContent>
-          <Typography>User Profile Works!</Typography>
-        </CardContent>
-      </CardCover>
-    </Card>
+    <Box>
+      <Stack flexDirection={"row"}>
+        <Avatar
+          sx={{ width: { md: "200px" }, height: { md: "270px" } }}
+          variant="outlined"
+          //   alt="ACCOUNT NAME"
+        ></Avatar>
+        <Box flexGrow={1}>
+          <Stack justifyContent={"center"} alignItems={"center"} gap={5}>
+            <Typography fontSize={34}>Testerson McTester</Typography>
+            <Typography startDecorator={<Email />}>test@example.com</Typography>
+            <Typography startDecorator={<Phone />}>
+              +1 (555) 555-5555
+            </Typography>
+            <Typography startDecorator={<MyLocation />}>
+              United States
+            </Typography>
+          </Stack>
+        </Box>
+      </Stack>
+    </Box>
   );
 }
