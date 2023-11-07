948d044916555f3ecf39b7a2c0d22d7950f71c7d
Billy McCrackin <44419353+bmc1995@users.noreply.github.com>
2023-11-07 16:09:33 -0500

    feat(navbar): add tests

--- src/features/navbar/test/navigationBar.test.tsx
+++ src/features/navbar/test/navigationBar.test.tsx
@@ -1,3 +1,36 @@
-import { describe } from "vitest";
+import { render, screen } from "@testing-library/react";
+import { describe, expect, it, vi } from "vitest";
+import NavigationBar from "../components/NavigationBar";
+import { ModeToggle } from "../../../common/Buttons/ModeToggle";
 
-describe.skip("<NavigationBar />");
+describe("<NavigationBar />", () => {
+  vi.importMock<typeof ModeToggle>("../../../common/Buttons/ModeToggle");
+  vi.mock("@mui/joy", async () => {
+    const actual = await vi.importActual<{}>("@mui/joy");
+
+    return {
+      ...actual,
+      CssVarsProvider: ({ children }: { children: React.ReactNode }) => {
+        <div>{children}</div>;
+      },
+      useColorScheme: () => ({
+        mode: "system",
+        setMode: vi.fn(),
+      }),
+    };
+  });
+
+  it("has a parent nav element", () => {
+    render(<NavigationBar />);
+
+    const nav = screen.getByLabelText("Vreact Template");
+    expect(nav.tagName).toBe("NAV");
+  });
+  it("has a 'Home' navlink", () => {
+    render(<NavigationBar />);
+
+    const homeBtn = screen.getByLabelText("Home");
+    expect(homeBtn).toBeVisible();
+    expect(homeBtn).toHaveAttribute("role", "menuitem");
+  });
+});
