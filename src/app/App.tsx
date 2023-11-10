e342bea44e345a919728a601ef8ef59b3a1665b8
Billy McCrackin <44419353+bmc1995@users.noreply.github.com>
2023-11-09 19:21:22 -0500

    fix(pre-commit): test hook & fix package.json :)

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,15 @@
-import "../styles/App.css";
-import ErrorBoundary from "../features/error-boundary/components/ErrorBoundary";
-import { Layout } from "../common/Layout/Layout";
-import { CssBaseline, CssVarsProvider } from "@mui/joy";
-import { NetworkDetector } from "../features/network-detector/NetworkDetector";
+import '../styles/App.css';
+import ErrorBoundary from '../features/error-boundary/components/ErrorBoundary';
+import { Layout } from '../common/Layout/Layout';
+import { CssBaseline, CssVarsProvider } from '@mui/joy';
+import { NetworkDetector } from '../features/network-detector/NetworkDetector';
 // import { SignUpPage } from "../features/auth/pages/SignUpPage";
-import { ToastContainer } from "../common/notifications/ToastContainer";
-// import { LoginPage } from "../features/auth/pages/LoginPage";
-import UserDashboard from "../features/user/components/UserDashboard";
+import { ToastContainer } from '../common/notifications/ToastContainer';
+// import { LoginPage } from '../features/auth/pages/LoginPage';
+import UserDashboard from '../features/user/components/UserDashboard';
 function App() {
   return (
-    <CssVarsProvider defaultMode={"system"}>
+    <CssVarsProvider defaultMode={'system'}>
       <CssBaseline>
         <NetworkDetector>
           <ErrorBoundary fallback={<h1>Something went wrong</h1>}>
