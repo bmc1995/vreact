8ac4b786a1ae1bb6cca5dd79880dcb56975b2920
Billy McCrackin <44419353+bmc1995@users.noreply.github.com>
2023-11-13 18:54:44 -0500

    ref(router): add non data router style

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,18 +3,30 @@ import ErrorBoundary from '../features/error-boundary/components/ErrorBoundary';
 import { Layout } from '../common/Layout/Layout';
 import { CssBaseline, CssVarsProvider } from '@mui/joy';
 import { NetworkDetector } from '../features/network-detector/NetworkDetector';
+// import { SignUpPage } from "../features/auth/pages/SignUpPage";
 import { ToastContainer } from '../common/notifications/ToastContainer';
 
-function App({ children }: { children: React.ReactNode }) {
+import UserDashboard from '../features/user/components/UserDashboard';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { LoginPage } from '../features/auth/pages/LoginPage';
+import { SignUpPage } from '../features/auth/pages/SignUpPage';
+
+function App() {
   return (
     <CssVarsProvider defaultMode={'system'}>
       <CssBaseline>
         <NetworkDetector>
           <ErrorBoundary fallback={<h1>Something went wrong</h1>}>
-            <Layout>
-              <ToastContainer />
-              {children}
-            </Layout>
+            <ToastContainer />
+            <BrowserRouter>
+              <Routes>
+                <Route element={<Layout />}>
+                  <Route path='/' element={<LoginPage />} />
+                  <Route path='/signup' element={<SignUpPage />} />
+                  <Route path='/dashboard' element={<UserDashboard />} />
+                </Route>
+              </Routes>
+            </BrowserRouter>
           </ErrorBoundary>
         </NetworkDetector>
       </CssBaseline>
--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,12 @@ import ReactDOM from 'react-dom/client';
 import App from './app/App.tsx';
 import { Provider } from 'react-redux';
 import store from './app/redux/store.tsx';
-import { RouterProvider } from 'react-router-dom';
 import './index.css';
-import { router } from './app/router.tsx';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App>
-        <RouterProvider router={router} />
-      </App>
+      <App />
     </Provider>
   </React.StrictMode>,
 );
