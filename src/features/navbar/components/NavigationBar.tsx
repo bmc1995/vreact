662b2a0e53c76483b2d1df167179476a931286a3
Billy McCrackin <44419353+bmc1995@users.noreply.github.com>
2023-11-12 22:32:40 -0500

    fix(navbar): fixed padding on hover

--- src/features/navbar/components/NavigationBar.tsx
+++ src/features/navbar/components/NavigationBar.tsx
@@ -1,61 +1,42 @@
-import Box from "@mui/joy/Box";
-import List from "@mui/joy/List";
-import ListDivider from "@mui/joy/ListDivider";
-import ListItem from "@mui/joy/ListItem";
-import ListItemButton from "@mui/joy/ListItemButton";
-import Person from "@mui/icons-material/Person";
-import viteLogo from "/vite.svg";
-import { ModeToggle } from "../../../common/Buttons/ModeToggle";
+import Box from '@mui/joy/Box';
+import List from '@mui/joy/List';
+import ListDivider from '@mui/joy/ListDivider';
+import ListItem from '@mui/joy/ListItem';
+import ListItemButton from '@mui/joy/ListItemButton';
+import Person from '@mui/icons-material/Person';
+import viteLogo from '/vite.svg';
+import { ModeToggle } from '../../../common/Buttons/ModeToggle';
 
 export default function NavigationBar() {
   return (
     <Box
-      component="nav"
-      aria-label="Vreact Template"
+      component='nav'
+      aria-label='Vreact Template'
       sx={{
         flexGrow: 1,
       }}
     >
-      <List
-        role="menubar"
-        orientation="horizontal"
-        sx={{ padding: 0, marginRight: ".75rem", height: "75px" }}
-      >
-        <ListItem role="none">
-          <ListItemButton
-            role="menuitem"
-            component="a"
-            href="#NavHomeBtn"
-            aria-label="Home"
-          >
-            <img src={viteLogo} className="logo" alt="Vite logo" />
+      <List role='menubar' orientation='horizontal' sx={{ padding: 0, marginRight: '.75rem', height: '75px' }}>
+        <ListItem role='none'>
+          <ListItemButton role='menuitem' component='a' href='#NavHomeBtn' aria-label='Home'>
+            <img src={viteLogo} className='logo' alt='Vite logo' />
           </ListItemButton>
         </ListItem>
-        <ListItem role="none" sx={{ marginInlineStart: "auto" }}>
-          <ListItemButton role="menuitem" component="a" href="#NavBtn">
+        <ListItem role='none' sx={{ marginInlineStart: 'auto' }}>
+          <ListItemButton role='menuitem' component='a' href='#NavBtn'>
             Products
           </ListItemButton>
         </ListItem>
         <ListDivider sx={{ margin: 0 }} />
-        <ListItem role="none" sx={{ minWidth: "100px" }}>
-          <ListItemButton
-            role="menuitem"
-            component="a"
-            sx={{ justifyContent: "center" }}
-            href="#NavBtn"
-          >
+        <ListItem role='none' sx={{ minWidth: '100px' }}>
+          <ListItemButton role='menuitem' component='a' sx={{ justifyContent: 'center' }} href='#NavBtn'>
             Blog
           </ListItemButton>
         </ListItem>
         <ListDivider sx={{ margin: 0 }} />
-        <ListItem role="none" sx={{ padding: 0 }}>
+        <ListItem role='none' sx={{ paddingX: 0 }}>
           <ModeToggle />
-          <ListItemButton
-            role="menuitem"
-            component="a"
-            href="#NavBtn"
-            aria-label="Profile"
-          >
+          <ListItemButton role='menuitem' component='a' href='#NavBtn' aria-label='Profile'>
             <Person />
           </ListItemButton>
         </ListItem>
