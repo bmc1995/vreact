d40595e7576e0af0b67fa9d592481df05d6a0715
Billy McCrackin <44419353+bmc1995@users.noreply.github.com>
2023-11-13 17:21:08 -0500

    feat(main): add router provider

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client';
 import App from './app/App.tsx';
 import { Provider } from 'react-redux';
 import store from './app/redux/store.tsx';
+import { RouterProvider } from 'react-router-dom';
 import './index.css';
+import { router } from './app/router.tsx';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <App>
+        <RouterProvider router={router} />
+      </App>
     </Provider>
   </React.StrictMode>,
 );
