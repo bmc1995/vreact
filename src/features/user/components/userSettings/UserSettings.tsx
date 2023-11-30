4ab7051581196abfd77a46100c22553a1b252f6f
Billy McCrackin <44419353+bmc1995@users.noreply.github.com>
2023-11-29 21:54:17 -0700

    fix(forms): correct import paths

--- src/features/user/components/userSettings/UserSettings.tsx
+++ src/features/user/components/userSettings/UserSettings.tsx
@@ -1,5 +1,5 @@
 import { Sheet } from '@mui/joy';
-import { UserSettingsForm } from './forms/UserSettingsForm';
+import { UserSettingsForm } from '../forms/UserSettingsForm';
 
 export default function UserSettings() {
   return (
