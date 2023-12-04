c433eea8a24cc66c64d9ea6cea4aa7e47e9afa24
Billy McCrackin <44419353+bmc1995@users.noreply.github.com>
2023-12-04 12:07:30 -0700

    ref(fake-auth): add jsdoc & roles

--- src/app/routing/fakeauthprovider.ts
+++ src/app/routing/fakeauthprovider.ts
@@ -1,26 +1,58 @@
-import { login, logout } from '../redux/slices/userSlice';
-import store, { RootState } from '../redux/store';
-
+import { ROLES } from '../../features/user/utils/zod/roles';
+import { IUser, login, logout } from '../redux/slices/userSlice';
+import store from '../redux/store';
+/**
+ * Represents an authentication provider.
+ */
 interface AuthProvider {
+  /**
+   * Checks if the user is authenticated.
+   * @returns A boolean indicating whether the user is authenticated.
+   */
   isAuthenticated: () => boolean;
-  getUser: () => RootState['user'];
+
+  /**
+   * Retrieves the user information.
+   * @returns The user information.
+   */
+  getUser(): IUser;
+
+  /**
+   * Signs in the user with the provided email and password.
+   * @param email - The user's email.
+   * @param password - The user's password.
+   * @returns A promise that resolves when the sign-in process is complete.
+   */
   signin({ email, password }: { email: string; password: string }): Promise<void>;
+
+  /**
+   * Signs out the user.
+   * @returns A promise that resolves when the sign-out process is complete.
+   */
   signout(): Promise<void>;
 }
 
 /**
- * This represents some generic auth provider API
+ * A fake authentication provider that implements the AuthProvider interface.
  */
 export const fakeAuthProvider: AuthProvider = {
   isAuthenticated: () => store.getState().user.isAuthenticated,
   getUser: () => store.getState().user,
-  async signin({ email }: { email: string; password: string }) {
+
+  async signin({ email, password }: { email: string; password: string }) {
     await new Promise(r => setTimeout(r, 500)); // fake delay
 
-    // if (password === 'supersecretpassword') {
-    store.dispatch(login({ isAuthenticated: true, email, id: 'qwerty:)' }));
-    // }
+    const fakeUser = { isAuthenticated: true, email, id: 'qwerty:)', role: ROLES.REGULAR };
+
+    if (password === 'regularpassword') {
+      store.dispatch(login(fakeUser));
+    } else if (password === 'adminpassword') {
+      store.dispatch(login({ ...fakeUser, role: ROLES.ADMIN }));
+    } else {
+      throw new Error('Invalid credentials');
+    }
   },
+
   async signout() {
     await new Promise(r => setTimeout(r, 500)); // fake delay
     store.dispatch(logout());
