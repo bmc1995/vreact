b0de8d7d5cb6e13994bccee2605f81b9566df55c
Billy McCrackin <44419353+bmc1995@users.noreply.github.com>
2023-12-07 13:58:28 -0700

    fix(useRbac): rm useSelector hook

--- src/features/rbac/useRbac.ts
+++ src/features/rbac/useRbac.ts
@@ -1,9 +1,8 @@
-import { useSelector } from 'react-redux';
-import { RootState } from '../../app/redux/store';
+import store from '../../app/redux/store';
 import { RoleType } from '../../common/models/role';
 
 export const useRBAC = (component: React.ReactNode, validRoles: RoleType[]) => {
-  const role = useSelector((state: RootState) => state.auth.user?.role || '');
+  const role = store.getState().auth.user?.role || '';
 
   if (validRoles.includes(role)) {
     return component;
